6725bb4fcf341e7b6ca6a619f8802eef69a8b75d
Nikita Samoilov <nikisamoylov@gmail.com>
2023-04-07 15:18:28 +0400

    init commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+class MobileBankApiTestV4 {
+    @Test
+    void shouldReturnDemoAccounts() {
+      // Given - When - Then
+      // Предусловия
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/demo/accounts")
+      // Проверки
+              // код теста
+              .then()
+              .statusCode(200)
+              // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+      ;
+      ;
+    }
+}
